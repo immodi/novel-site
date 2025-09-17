d00f7f3a044bb39ee5a96c61c71e48653b725ced
immodi <ay727468a@gmail.com>
2025-09-17 14:13:41 +0300

    forgot this

--- internal/http/templates/layout_templ.go
+++ internal/http/templates/layout_templ.go
@@ -34,7 +34,7 @@ func Layout(metaData *indexdtostructs.MetaDataStruct, data *indexdtostructs.Layo
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/static/styles/output.css\"><link rel=\"icon\" type=\"image/svg+xml\" href=\"/static/media/logo/logo_icon.svg\"><script src=\"/static/js/htmx.min.js\"></script><title>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/static/styles/output.css\"><link rel=\"icon\" type=\"image/svg+xml\" href=\"/static/logo/logo_icon.svg\"><script src=\"/static/js/htmx.min.js\"></script><title>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
